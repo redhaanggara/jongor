63664ae7207787aa53ca49d1dbded538694a9b09
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-11-25 17:57:40 +0700

    Update window.php

--- application/views/window.php
+++ application/views/window.php
@@ -86,7 +86,7 @@
 	    		<td><?php echo $row->prestasi?></td>
 	    		<td><?php echo $row->alamat?></td>
 	    		<td><?php echo $row->status?></td>
-	    		<td><a href="http://ibgf.azurewebsites.net/index.php/person/admin_update_pendaftar?id=<?php echo  $row->id; ?>" class="btn btn-primary" class="btn btn-primary">Update</a> <a href="http://localhost/master/person/admin_delete_pendaftar?id=<?php echo  $row->id; ?>" class="btn btn-danger">Delete</a></td>
+	    		<td><a href="http://ibgf.azurewebsites.net/index.php/person/admin_update_pendaftar?id=<?php echo  $row->id; ?>" class="btn btn-primary" class="btn btn-primary">Update</a> <a href="http://ibgf.azurewebsites.net/index.php/person/admin_delete_pendaftar?id=<?php echo  $row->id; ?>" class="btn btn-danger">Delete</a></td>
 	    	
 	    	</tr>
 	    </tbody>
