0708995c713ecd0779e74a9c641d4d06e3f16338
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-11-25 17:42:36 +0700

    Update register_update_view.php

--- application/views/register_update_view.php
+++ application/views/register_update_view.php
@@ -20,7 +20,7 @@
   	<div class="row">
 			<div class="control-group" id="fields">
 				<div class="controls">
-					<form role="form" autocomplete="off" action="http://localhost/master/person/update_insert_data?id=<?php echo  $key->id; ?>" method="post">
+					<form role="form" autocomplete="off" action="http://ibgf.azurewebsites.net/index.php/person/update_insert_data?id=<?php echo  $key->id; ?>" method="post">
 						<div class="form-group">
 							<label for="inputnama">Nama Lengkap</label>
 							<input type="text" name="nama" placeholder="Nama Legkap" class="form-control" value="<?php echo $key->nama; ?>">
