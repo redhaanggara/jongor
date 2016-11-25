22aeeda5d362b0e37a381dbf634675135f18a1ee
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-11-25 17:44:41 +0700

    Update succes_send.php

--- application/views/succes_send.php
+++ application/views/succes_send.php
@@ -17,7 +17,7 @@
 		    <div class="alert alert-success fade in">
 		        <a href="#" class="close" data-dismiss="alert">&times;</a>
 		        <strong>Success!</strong> Your PDF fie has been sent to your email successfully.
-		        <a href="http://localhost/master/main/home_page" class="btn btn-primary" data-dismiss="alert">&nbsp <span class="glyphicon glyphicon-home"></span>&nbsp back to home</a>
+		        <a href=http://ibgf.azurewebsites.net/index.php/main/home_page" class="btn btn-primary" data-dismiss="alert">&nbsp <span class="glyphicon glyphicon-home"></span>&nbsp back to home</a>
 		    </div>
 		</div>
 	</center>
